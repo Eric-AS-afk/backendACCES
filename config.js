d253ae35cc02fb460b79ba28e14f9513aff50706
Eric Avila <eavilas5@miumg.edu.gt>
2025-11-12 20:25:27 -0600

    config

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 export const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:5173";
 
-export const DB_HOST = process.env.DB_HOST || "127.0.0.1";
+export const DB_HOST = process.env.DB_HOST || "66.29.148.12";
 export const DB_PORT = process.env.DB_PORT || 3306;
-export const DB_DATABASE = process.env.DB_DATABASE || "control_siena";
-export const DB_USER = process.env.DB_USER || "root";
-export const DB_PASSWORD = process.env.DB_PASSWORD || "password";
+export const DB_DATABASE = process.env.DB_DATABASE || "qmbmwsjkey_cgp_SIENA";
+export const DB_USER = process.env.DB_USER || "qmbmwsjkey_admin111";
+export const DB_PASSWORD = process.env.DB_PASSWORD || "Eric30David01";
 
 export const PORT = process.env.PORT || 3000;
