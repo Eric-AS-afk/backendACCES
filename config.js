4e636c4f5dd040f33c53438c43d6e18412011dc1
Eric Avila <eavilas5@miumg.edu.gt>
2025-11-13 21:28:59 -0600

    local

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 export const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:5173";
 
-export const DB_HOST = process.env.DB_HOST || "sienacces.site";
+export const DB_HOST = process.env.DB_HOST || "localhost";
 export const DB_PORT = process.env.DB_PORT || 3306;
 export const DB_DATABASE = process.env.DB_DATABASE || "qmbmwsjkey_cgp_SIENA";
 export const DB_USER = process.env.DB_USER || "qmbmwsjkey_admin111";
